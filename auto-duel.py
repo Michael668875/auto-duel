d66aa90ba1ca74336d7553c36df4a103e988a3c9
Michael668875 <159018843+Michael668875@users.noreply.github.com>
2026-02-12 01:14:12 +1100

    fixed timing for draw phase

--- auto-duel.py
+++ auto-duel.py
@@ -174,11 +174,10 @@ for i in range(total_duel):
             # Click ONLY if draw is visible AND we're allowed to click
             if allow_click and draw_visible and you_visible:
                 if click_and_confirm(find_img=folder+"draw_phase.png",
-                                    confirm_gone_img=folder+"draw_phase.png",
                                     confirm_appear_img=folder+"action.png",
-                                    timeout=1,
-                                    click_delay=0.1,
-                                    min_clicks=3):
+                                    timeout=3,
+                                    click_delay=0.2,
+                                    min_clicks=5):
                                     break
 
             time.sleep(CLICK_INTERVAL)    
